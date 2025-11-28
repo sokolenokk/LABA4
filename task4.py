03f2d3ac51e179c2e10c2f5a73c12913e727e99c
sokolenok <skls77@mail.ru>
2025-11-29 01:58:17 +0300

    Implement task 4

--- task4.py
+++ task4.py
@@ -10,14 +10,25 @@ def parse_club(line):
     return {
         'название': parts[0],
         'страна': parts[1],
-        'победы_лч': parts[2],
-        'год_основания': parts[3],
+        'победы_лч': int(parts[2]),
+        'год_основания': int(parts[3]),
         'стадион': parts[4]
     }
 
 
 def filter_clubs(filename, country, min_wins):
     for line in read_clubs(filename):
-        club = parse_club(line)
-        if club['страна'] == country and club['победы_лч'] > min_wins:
-            yield club
+        clubs = parse_club(line)
+        if clubs['страна'] == country and clubs['победы_лч'] > min_wins:
+            yield clubs
+
+
+print("Испаниские клубы, у которых больше трех побед в Лиге Чемпионов")
+print("-" * 62)
+
+for club in filter_clubs('football_clubs.csv', country='Испания', min_wins=3):
+    print(f"{club['название']}")
+    print(f"Год основания: {club['год_основания']}")
+    print(f"Стадион: {club['стадион']}")
+    print(f"Побед в ЛЧ: {club['победы_лч']}")
+    print('^' * 30)
