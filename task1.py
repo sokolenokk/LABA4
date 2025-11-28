79b0f8ebac3d2b76a3e2d8b47e3e3c15a2f4fb4d
sokolenok <skls77@mail.ru>
2025-11-28 22:56:25 +0300

    Add prime generator function

--- task1.py
+++ task1.py
@@ -5,3 +5,11 @@ def prime_number(n):
         if n % i == 0:
             return False
     return True
+
+
+def prime_generator(limit=None):
+    num = 2
+    while limit is None or num <= limit:
+        if prime_number(num):
+            yield num
+        num +=1
