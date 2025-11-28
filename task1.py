6e7edc99f5b3d5ec82be27eaaccab3f7f01fba40
sokolenok <skls77@mail.ru>
2025-11-28 23:10:41 +0300

    Implement task 1

--- task1.py
+++ task1.py
@@ -1,7 +1,7 @@
 def prime_number(n):
     if n < 2:
         return False
-    for i in range(2, int(n**5) + 1):
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
     return True
@@ -12,4 +12,14 @@ def prime_generator(limit=None):
     while limit is None or num <= limit:
         if prime_number(num):
             yield num
-        num +=1
+        num += 1
+
+
+print("Первое 21 простое число:")
+gen = prime_generator()
+for _ in range(21):
+    print(next(gen), end=" | ")
+print()
+
+print("\nВсе простые числа до 77:")
+print(list(prime_generator(77)))
